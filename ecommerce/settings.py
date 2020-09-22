d390bea6f4d7d51d68be747b62fc8fc07effdfbf
MYaseenK20 <boy20122001@gmail.com>
2020-09-22 15:30:32 +0500

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,5 +132,4 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/image')
 
 
-django_heroku.settings(locals())
 
